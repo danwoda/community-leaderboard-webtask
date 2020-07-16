dbe2cfc5ba36dc9d808af4bddb179f38efc3859a
IdentityDan <dan.woda@auth0.com>
2020-07-16 16:40:05 -0700

    added basic function

---
+++ app.js
@@ -0,0 +1,43 @@
+module.exports = async (ctx, cb) => {
+
+  const axios = require('axios')
+
+  async function getLeaderboardRows() {
+
+    const config = {
+      baseURL: 'https://community.auth0.com',
+      url: '/admin/plugins/explorer/queries/133/run',
+      method: 'POST',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'multipart/form-data',
+        'Api-Key': ctx.secrets.apiKey,
+        'Api-Username': ctx.secrets.apiUsername
+      }
+    }
+
+    try {
+      const response = await axios(config)
+      return response.data.rows
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  const leaderboardRows = await getLeaderboardRows()
+  const leaderboardTopThree = leaderboardRows.slice(0, 3)
+  const leaderboard = leaderboardTopThree.map( row => ({ name: row[1], email: row[2], score: row[6] }))
+
+  const response = `
+  🏆 DSE Community Leaderboard
+
+  🥇 ${leaderboard[0].name} (${leaderboard[0].email}) - ${leaderboard[0].score} pts
+
+  🥈 ${leaderboard[1].name} (${leaderboard[1].email}) - ${leaderboard[1].score} pts
+
+  🥉 ${leaderboard[2].name} (${leaderboard[2].email}) - ${leaderboard[2].score} pts`
+
+
+  cb(null, { text: response })
+}
