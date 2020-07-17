dd2362d7bb7d36ba9ca8b16055c0fbd27922c366
IdentityDan <dan.woda@auth0.com>
2020-07-16 17:42:11 -0700

    Added user input

--- app.js
+++ app.js
@@ -22,24 +22,33 @@ module.exports = async (ctx, cb) => {
     }
     catch (error) {
       console.error(error)
-      cb(null, { text: `¯\\_(ツ)_/¯  Oops...there was an error.` })
+      cb(null, { text: `¯\\_(ツ)_/¯  Oops...there was an error. Please contact the Community Team for more info.` })
     }
   }
 
   const leaderboardRows = await getLeaderboardRows()
+
+  if(ctx.body.text) {
+    const email = ctx.body.text
+    const matchingRow = leaderboardRows.find(i => i[2] === email)
+    const response = matchingRow ? `🏆 DSE Community Leaderboard\n\n ${matchingRow[1]} (${matchingRow[2]}) currently has ${matchingRow[6]} pts` : `We couldn't find an email address matching \`${email}\`.`
+    cb(null, { text: response })
+  }
+
   const leaderboardTopThree = leaderboardRows.slice(0, 3)
   const leaderboard = leaderboardTopThree.map( row => ({ name: row[1], email: row[2], score: row[6] }))
 
   const response = `
-  🏆 Your Community Leaderboard
+  🏆 DSE Community Leaderboard
 
   🥇 ${leaderboard[0].name} (${leaderboard[0].email}) - ${leaderboard[0].score} pts
 
   🥈 ${leaderboard[1].name} (${leaderboard[1].email}) - ${leaderboard[1].score} pts
 
-  🥉 ${leaderboard[2].name} (${leaderboard[2].email}) - ${leaderboard[2].score} pts`
+  🥉 ${leaderboard[2].name} (${leaderboard[2].email}) - ${leaderboard[2].score} pts
 
+  To see the score for a specific email, use \`/wt dse-community-leaderboard {email}\``
 
-  cb(null, { text: response })
+  cb(null, { text: response, response_type: 'in_channel' })
 }
 
